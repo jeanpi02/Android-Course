369b7004f295bc38161be9fe32fa0c8a3592d3d3
unknown <jeanpvargas18@gmail.com>
2024-08-10 19:23:36 -0500

    Update

---
+++ app/src/main/java/com/jeanpi/androidmaster/firstapp/FirstAppActivity.kt
@@ -0,0 +1,42 @@
+package com.jeanpi.androidmaster.firstapp
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.jeanpi.androidmaster.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_first_app)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnStart = findViewById<Button>(R.id.btnStart)
+        val etName1 = findViewById<EditText>(R.id.etName)
+
+
+
+
+
+        btnStart.setOnClickListener {
+            val name = etName1.text.toString()
+            if(name.isNotEmpty()){
+                Log.i("Jeanpi" , "Button pulsado ${name}")
+            }
+
+
+        }
+
+
+    }
+}
